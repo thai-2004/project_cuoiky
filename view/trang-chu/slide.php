0fc526b49b93d02bd0237122a5af47dbac489ba2
thai-2004 <thai1042004@gmail.com>
2025-05-04 22:07:58 +0700

    Cap nhat trang chu, admin, anh

---
+++ view/trang-chu/slide.php
@@ -0,0 +1,44 @@
+ <!-- Hero Section Start -->
+ <div class="hero-slider hero-slider-one">
+     <!-- Single Slide Start -->
+     <div class="single-slide" style="background-image: url(assets/images/slider/slide-bg-5.jpg)">
+         <!-- Hero Content One Start -->
+         <div class="hero-content-one container position-relative h-100">
+             <div class="slide-content-bottom position-absolute">
+                 <div class="col-lg-12 col-md-12">
+                     <div class="slider-content-text text-left">
+                         <h2>Giảm giá độc quyền 30/4</h2>
+                         <p>Khi nhập voucher 30 THANG 4</p>
+                         <div class="slide-btn-group">
+                             <a href="shop.html" class="btn btn-bordered btn-style-1">Mua ngay</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <!-- Hero Content One End -->
+     </div>
+     <!-- Single Slide End -->
+
+     <!-- Single Slide Start -->
+     <div class="single-slide" style="background-image: url(assets/images/slider/slide-bg-1.jpg)">
+         <!-- Hero Content One Start -->
+         <div class="hero-content-one container position-relative h-100">
+             <div class="slide-content-bottom position-absolute">
+                 <div class="col-lg-12 col-md-12">
+                     <div class="slider-content-text text-left">
+                         <h2>H-Vault Classico</h2>
+                         <p>Ưu đãi độc quyền - Giảm 20% trong tuần này</p>
+                         <p>Chỉ với giá: <strong>1.499.00đ</strong></p>
+                         <div class="slide-btn-group">
+                             <a href="shop.html" class="btn btn-bordered btn-style-1">Mua ngay</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <!-- Hero Content One End -->
+     </div>
+     <!-- Single Slide End -->
+ </div>
+ <!-- Hero Section End -->

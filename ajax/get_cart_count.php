a67a4bd0d6f9d8242bd5cd4acad3988d248043b8
Nguyễn Minh Thái <98826198+thai-2004@users.noreply.github.com>
2025-05-04 21:43:01 +0700

    Delete

--- ajax/get_cart_count.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/db.php';
-require_once '../config/functions.php';
-
-header('Content-Type: application/json');
-
-$count = get_cart_count();
-echo json_encode(['count' => $count]);
-?> 
--- ajax/get_cart_total.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/db.php';
-require_once '../config/functions.php';
-
-header('Content-Type: application/json');
-
-$total = get_cart_total();
-echo json_encode(['total' => format_price($total)]);
-?> 
